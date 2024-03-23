fa9cf4e01063c8487082c9d542186790be56127d
HarakeSenpai <mohamad.harakeh2001@gmail.com>
2024-03-23 22:44:21 +0200

    created repo and a simple signup / backend

---
+++ Backend/connection.php
@@ -0,0 +1,17 @@
+<?php
+header("Access-Control-Allow-Origin: http://127.0.0.1:5501");
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Allow-Headers: Content-Type");
+header("Access-Control-Allow-Origin: *");
+
+$host = "localhost";
+$db_user = "root";
+$db_pass = null;
+$db_name = "linkedindb";
+
+$mysqli = new mysqli($host, $db_user, $db_pass, $db_name);
+
+if ($mysqli->connect_error) {
+    die( $mysqli->connect_error);
+}
+?>
---
+++ Backend/signup.php
@@ -0,0 +1,69 @@
+<?php
+include ('connection.php');
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $password = $_POST["password"];
+
+    $hashed_password = password_hash($password, PASSWORD_BCRYPT);
+
+    $check_user = $mysqli->prepare('SELECT email FROM users WHERE email=?');
+    $check_user->bind_param('s', $email);
+    $check_user->execute();
+    $check_user->store_result();
+    $user_exists = $check_user->num_rows();
+
+    if ($user_exists > 0) {
+        $response["status"] = "error";
+        $response["message"] = "email already exists.";
+    } else {
+        $query = $mysqli->prepare('INSERT INTO users (name, email, password) VALUES (?,?,?)');
+        $query->bind_param('sss', $name, $email, $hashed_password);
+        $query->execute();
+
+        $response['status'] = "success";
+        $response['message'] = "User $email was created successfully.";
+    }
+    echo json_encode($response);
+}
+
+    elseif ($_SERVER['REQUEST_METHOD'] === 'GET') {
+        
+       
+
+        if (!isset($_GET['id'])) {
+            $response = [
+                'status' => 'error',
+                'message' => 'User ID is missing'
+            ];
+            echo json_encode($response);
+            exit;
+        }
+
+        $user_id = $_GET['id'];
+
+        $query = $mysqli->prepare('SELECT * FROM users WHERE id = ?');
+        $query->bind_param("i", $user_id);
+        $query->execute();
+        $result = $query->get_result();
+
+        if ($result->num_rows > 0) {
+            $userInfo = [];
+            while ($row = $result->fetch_assoc()) {
+                $userInfo[] = $row;
+            }
+            $response = [
+                'status' => 'success',
+                'user_info' => $userInfo
+            ];
+        } else {
+            $response = [
+                'status' => 'error',
+                'message' => 'No user found with the given ID'
+            ];
+        }
+        echo json_encode($response);
+    }
+
+
